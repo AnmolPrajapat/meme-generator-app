263a45b74603557cd195f0719b22e7aef82229f3
AnmolPrajapat <prajapatanmol2001@gmail.com>
2021-03-16 18:31:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Header from "./component/Header";
+import MainComponent from "./component/MainComponent"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                alt="Problem?"
+            />
+            <p style={{textAlign:'center'}}>Meme Generator</p>
+        </header>
+    )
+}
